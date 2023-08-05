0387aff47f0b87a677bea51b297936ebf7341df4
sonika tiwari <sonitiwari>
2023-08-05 18:09:18 +0100

    first commit

--- src/main/scala/movie1.scala
+++ src/main/scala/movie1.scala
@@ -11,12 +11,13 @@ object movie1 extends App{
   Logger.getLogger("org").setLevel(Level.ERROR)
   val sparkConf = new SparkConf()
   sparkConf.set("spark.app.name", "kaf")
-  //sparkConf.set("spark.master", "local[1]")
+  sparkConf.set("spark.master", "local[1]")
 
   val spark=SparkSession.builder().config(sparkConf).getOrCreate()
   import spark.implicits
-  val df = spark.read.option("header", true).option("inferschema", true).csv("/user/ec2-user/junbatch/sonika/topmovie.csv")
-      df.where(df("vote_average==8")).show()
+  val df = spark.read.option("header", false).option("inferschema", true).csv("C:\\Users\\A\\Desktop\\tmr\\data")
+  df.createOrReplaceTempView("movie")
+  val completedOrders = spark.sql("select * from movie ").show()
     df.show(5)
 
 
